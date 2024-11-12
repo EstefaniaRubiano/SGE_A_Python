eda6369ebdc089f1b365ff10aab19dd0b142b663
EstefaniaRubiano <rubianoestefania15@gmail.com>
2024-11-12 18:40:26 +0100

    ejercicio 2

---
+++ exercici2.py
@@ -0,0 +1,26 @@
+#crear una variable pel dia de la setmana i que el programa retorni quin dia es
+dia_setmana = "dijous"
+
+if dia_setmana == "dilluns":
+    print("Avui és dilluns.")
+
+elif dia_setmana == "dimarts":
+    print("Avui és dimarts.")
+
+elif dia_setmana == "dimecres":
+    print("Avui és dimecres.")
+
+elif dia_setmana == "dijous":
+    print("Avui és dijous.")
+
+elif dia_setmana == "divendres":
+    print("Avui és divendres.")
+
+elif dia_setmana == "dissabte":
+    print("Avui és dissabte.")
+
+elif dia_setmana == "diumenge":
+    print("Avui és diumenge.")
+
+else:
+    print("Dia incorrecte.")
